d0a058aec3cc72bc0cabde608fe5c9ee7bbed894
okCompiled <sazidkhan78636@gmail.com>
2023-08-12 23:09:13 +0530

    add seo

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Helmet } from "react-helmet";
 
-const Layout = (props) => {
+const Layout = ({ children, title, description, keywords, author }) => {
     return(
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="author" content={author} />
+                <title>{title}</title>
+            </Helmet>
             <Header></Header>
             <main style={{minHeight:"79vh"}}>
-                {props.children}
+                {children}
             </main>
             <Footer></Footer>
         </div>
     )
-}
+};
+
+Layout.defaultProps = {
+    title: "Ecommerce app - shop now",
+    description: "mern stack project",
+    keywords: "mern,react,node,mongodb",
+    author: "Sazid",
+  };
 
 export default Layout
