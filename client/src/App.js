adaea61490630ce7ca87bec6aff859e6a0933527
okCompiled <sazidkhan78636@gmail.com>
2023-08-12 17:17:50 +0530

    add basic default for react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <>
+    <Layout>
+      
       <h1>Ecommerce App</h1>
+    </Layout>
     </>
   );
 }
