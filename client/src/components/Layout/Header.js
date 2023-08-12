ab904b021fc46d92cd13609ce4a29f5f3cb566a1
okCompiled <sazidkhan78636@gmail.com>
2023-08-13 00:17:31 +0530

    maintain state depending upon local storage token

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { GiShoppingBag } from "react-icons/gi";
+import { NavLink, Link } from "react-router-dom";
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
 
 const Header = () => {
+  const [auth, setAuth] = useAuth();
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user: null,
+      token: "",
+    });
+    localStorage.removeItem("auth");
+    toast.success("Logout Successfully");
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -19,38 +30,48 @@ const Header = () => {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <NavLink to="/" className="navbar-brand" href="#">
-              <GiShoppingBag />
-              Ecommere App
-            </NavLink>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <Link to="/" className="navbar-brand">
+              🛒 Ecommerce App
+            </Link>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink
-                  to="/"
-                  className="nav-link active"
-                  aria-current="page"
-                  href="#"
-                >
+                <NavLink to="/" className="nav-link ">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to="/category" className="nav-link">
+                <NavLink to="/category" className="nav-link ">
                   Category
                 </NavLink>
               </li>
+              {!auth?.user ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink to="/register" className="nav-link">
+                      Register
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/login" className="nav-link">
+                      Login
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      onClick={handleLogout}
+                      to="/login"
+                      className="nav-link"
+                    >
+                      Logout
+                    </NavLink>
+                  </li>
+                </>
+              )}
               <li className="nav-item">
-                <NavLink to="/register" className="nav-link">
-                  Register
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/login" className="nav-link" href="#">
-                  Login
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/cart" className="nav-link" href="#">
+                <NavLink to="/cart" className="nav-link">
                   Cart (0)
                 </NavLink>
               </li>
