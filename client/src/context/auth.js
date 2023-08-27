14eefbdfe379a19c34d9bf9e5e48d5bb7bf4b4bf
okCompiled <sazidkhan78636@gmail.com>
2023-08-27 15:47:31 +0530

    final touches

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -2,13 +2,13 @@ import { useState, useEffect, useContext, createContext } from "react";
 import axios from "axios";
 
 const AuthContext = createContext();
-
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     user: null,
     token: "",
   });
 
+  //default axios
   axios.defaults.headers.common["Authorization"] = auth?.token;
 
   useEffect(() => {
--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 import colors from "colors";
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Connected to MongoDB Database ${conn.connection.host}`.bgMagenta.white
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log(`Error in Mongodb ${error}`.bgRed.white);
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
   }
 };
 
